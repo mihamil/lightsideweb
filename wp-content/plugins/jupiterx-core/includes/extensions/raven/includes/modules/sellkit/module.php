96fe3ca759fac841f53669cc4c0763f11747a00b
Michael Hamilton <michael@lightsideweb.com>
2023-12-10 09:50:46 -0500

    Initial Commit

---
+++ wp-content/plugins/jupiterx-core/includes/extensions/raven/includes/modules/sellkit/module.php
@@ -0,0 +1,40 @@
+<?php
+namespace JupiterX_Core\Raven\Modules\Sellkit;
+
+defined( 'ABSPATH' ) || die();
+
+use JupiterX_Core\Raven\Base\Module_base;
+
+class Module extends Module_Base {
+	/**
+	 * Check if Woocommerce plugin is active.
+	 *
+	 * @return bool
+	 */
+	public static function is_active() {
+		return function_exists( 'WC' );
+	}
+
+	public function get_widgets() {
+		$widgets = [];
+
+		if ( ! function_exists( 'sellkit' ) ) {
+			$widgets = [
+				'sellkit-checkout-preview',
+				'sellkit-order-cart-details-preview',
+				'sellkit-order-details-preview',
+				'sellkit-personalised-coupons-preview',
+				'sellkit-product-filter-preview',
+			];
+		}
+
+		if ( function_exists( 'sellkit' ) && ! function_exists( 'sellkit_pro' ) ) {
+			$widgets = [
+				'sellkit-personalised-coupons-preview',
+				'sellkit-product-filter-preview',
+			];
+		}
+
+		return $widgets;
+	}
+}
